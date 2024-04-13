75b58300b45432593b9772b8b0141ae1124df833
yan0f <31446439+yan0f@users.noreply.github.com>
2024-04-14 00:29:03 +0300

    Init

---
+++ src/main.rs
@@ -0,0 +1,97 @@
+use std::{env, error::Error};
+
+use chrono::Local;
+use dotenv::dotenv;
+use teloxide::dptree::case;
+use teloxide::{
+    net::Download,
+    types::{ChatAction, InputFile, Me},
+};
+use teloxide::{prelude::*, Bot};
+use tokio::fs;
+
+async fn start(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let rules = "ПРАВИЛА:
+видео не должно быть больше 360p
+видео должно быть квадратным
+видео должно быть короче 60 секунд
+видео не должно быть тяжелее 8 мегабайт";
+    let text;
+    if let Some(first_name) = msg.chat.first_name() {
+        text = format!("привет, {first_name}, пришли видео\n{rules}");
+    } else {
+        text = format!("привет, пришли видео\n{rules}");
+    }
+    bot.send_message(msg.chat.id, text).await?;
+    Ok(())
+}
+
+async fn handle_video_message(bot: Bot, msg: Message, _: Me) -> ResponseResult<()> {
+    let video = msg.video().unwrap();
+
+    if video.width > 640 || video.height > 640 {
+        bot.send_message(msg.chat.id, "видео не должно быть больше 360p:(")
+            .await?;
+    } else if video.width != video.height {
+        bot.send_message(msg.chat.id, "видео должно быть квадратным:(").await?;
+    } else if video.duration > 60 {
+        bot.send_message(msg.chat.id, "видео должно быть короче 60 секунд:(")
+            .await?;
+    } else if (video.file.size / 1024 / 1024) > 8 {
+        bot.send_message(msg.chat.id, "видео не должно быть тяжелее 8 мегабайт:(")
+            .await?;
+    } else {
+        let now = Local::now();
+        let file = bot.get_file(&video.file.id).await?;
+        let file_path = msg.chat.username().unwrap_or_default();
+
+        let path = format!("./videos/{file_path}-{now}.mp4");
+        let mut dst = fs::File::create(&path).await?;
+        bot.download_file(&file.path, &mut dst).await?;
+
+        let video_note = InputFile::file(path);
+        bot.send_chat_action(msg.chat.id, ChatAction::UploadVideoNote).await?;
+        bot.send_video_note(msg.chat.id, video_note).await.expect("");
+    }
+
+    Ok(())
+}
+
+async fn handle_non_videos(bot: Bot, msg: Message, _: Me) -> ResponseResult<()> {
+    bot.send_message(msg.chat.id, "не понял тебя. пришли видео!").await?;
+
+    Ok(())
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase")]
+enum Command {
+    #[command(description = "show help.")]
+    Start,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+
+    fs::create_dir_all("videos").await?;
+
+    let bot_token = env::var("TELOXIDE_TOKEN").expect("TELOXIDE_TOKEN is not set");
+    let bot = Bot::new(bot_token);
+
+    let command_handler = teloxide::filter_command::<Command, _>().branch(case![Command::Start].endpoint(start));
+
+    let handler = dptree::entry().branch(
+        Update::filter_message()
+            .branch(command_handler)
+            .branch(dptree::filter(|msg: Message| msg.video().is_some()).endpoint(handle_video_message))
+            .endpoint(handle_non_videos),
+    );
+
+    Dispatcher::builder(bot, handler)
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+    Ok(())
+}
